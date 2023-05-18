029cad1dfb12d968452c673e878b346af0aa02bd
Denis Homolík <alfonz@homolik.cz>
2023-05-18 16:04:15 +0200

    add sentry

--- fow/fow/fowApp.swift
+++ fow/fow/fowApp.swift
@@ -1,11 +1,24 @@
 import SwiftUI
 import CoreLocation
 import Combine
+import Sentry
 
 
 @main
 struct fowApp: App {
     @ObservedObject private var viewModel = LocationsViewModel()
+    
+    init() {
+        SentrySDK.start { options in
+            options.dsn = "https://beeea9a05bc64650a646cc595f72dcc6@o4505120852606976.ingest.sentry.io/4505205360885760"
+            
+            options.enablePreWarmedAppStartTracing = true
+            options.attachScreenshot = true
+            options.attachViewHierarchy = true
+            options.enableMetricKit = true
+            options.enableCoreDataTracing = false
+        }
+    }
     
     var body: some Scene {
         WindowGroup {
